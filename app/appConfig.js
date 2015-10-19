967f8ed9c0fa965a1040d09f47076d3e2f05e1c5
davija <davija@cshp.info>
2015-10-19 16:03:04 -0700

    adding event filter class along with filter methods to determine if an event is a leave event or if the event is an all-day event.

--- app/appConfig.js
+++ app/appConfig.js
@@ -5,6 +5,7 @@ const config = {
   'database': {
     'connectionString': 'mongodb://localhost:27017/gcal-leave-scraper',
   },
+  'leaveKeywords': ['ooo', 'leave', 'off', 'vacation', 'holiday'],
 };
 
 module.exports = config;
