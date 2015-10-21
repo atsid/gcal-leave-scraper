ca219723ab620e37a662c6bd2954b191388323ee
davija <davija@cshp.info>
2015-10-21 14:57:48 -0700

    fixing lint error in test

--- app/persistence/models/GmailUser/index.spec.js
+++ app/persistence/models/GmailUser/index.spec.js
@@ -3,7 +3,6 @@ const GmailUser = require('../../../persistence').models.GmailUser;
 
 describe('GmailUser models', () => {
   describe('displayName method', () => {
-
     it('returns correctly formatted name', () => {
       const testUser = new GmailUser({'id': 'id1', 'email': 'test@example.com', 'firstName': 'fn', 'lastName': 'ln'});
 
