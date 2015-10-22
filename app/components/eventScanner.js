0df5d18226ecee108565687245260c58c93e901d
davija <davija@cshp.info>
2015-10-22 09:23:38 -0700

    adding comments to event scanner

--- app/components/eventScanner.js
+++ app/components/eventScanner.js
@@ -6,6 +6,7 @@ const eventFilter = new EventFilter([filters.allDayEventFilter, filters.leaveEve
 const Promise = require('bluebird');
 const LeaveEvent = Promise.promisifyAll(require('../persistence').models.LeaveEvent);
 const updateLeaveEvent = Promise.method((leaveEvent, leaveEventObj) => {
+  // Handles updating of an event that already exists.
   leaveEventObj.status = leaveEvent.status;
   leaveEventObj.summary = leaveEvent.summary;
   leaveEventObj.startDate = leaveEvent.startDate;
@@ -14,6 +15,7 @@ const updateLeaveEvent = Promise.method((leaveEvent, leaveEventObj) => {
   return Promise.method(leaveEventObj.save)();
 });
 const removeLeaveEvent = Promise.method((leaveEventObj) => {
+  // Handles removal of an event that exists but was cancelled/deleted.
   // Wrapping in a promise instead of using Promise.method due possible scoping bug in bluebird/mongoose.
   return new Promise((resolve) => {
     leaveEventObj.remove(() => {
@@ -22,9 +24,16 @@ const removeLeaveEvent = Promise.method((leaveEventObj) => {
   });
 });
 const saveLeaveEvent = Promise.method((leaveEvent) => {
+  // Handles creation of a new event
   return Promise.method(leaveEvent.save)();
 });
 
+/**
+ * Determines what operation to perform on the passed in event.
+ *
+ * @param leaveEvent Leave event to dispatch to correct handler
+ * @returns {Promise.<T>|*}
+ */
 function checkSaveUpdateOrRemove(leaveEvent) {
   return LeaveEvent.findAsync({'id': leaveEvent.id}).then((leaveEventObj) => {
     if (leaveEventObj.length && leaveEvent.status !== 'cancelled') {
@@ -40,6 +49,12 @@ function checkSaveUpdateOrRemove(leaveEvent) {
   });
 }
 
+/**
+ * Handler to process each leave event.
+ *
+ * @param event Event to process
+ * @returns {*}
+ */
 function calendarEventHandler(event) {
   return eventFilter.apply([event]).map((calLeaveEvent) => {
     return leaveEventTransformer(calLeaveEvent).then((leaveEvent) => {
@@ -48,6 +63,15 @@ function calendarEventHandler(event) {
   }).map(checkSaveUpdateOrRemove);
 }
 
+/**
+ * Iterates through a list of users and processes events in the specified date range.
+ *
+ * @param users List of users to process events on
+ * @param startDate Starting date of events to search for
+ * @param endDate Ending date of events to search for
+ * @param auth Google OAuth client
+ * @returns {Array}
+ */
 function scan(users, startDate, endDate, auth) {
   return Promise.map(users, (user) => {
     return helper.processEventsForUser(user.email, startDate, endDate, calendarEventHandler, auth);
