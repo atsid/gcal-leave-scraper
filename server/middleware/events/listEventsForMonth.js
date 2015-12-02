bbbf104221444fa3113fdb4842b1426dd0f5f84d
Jon McLean <jon.mclean@atsid.com>
2015-12-02 15:41:55 -0800

    Moved logic into a helper class so that it can be tested.  #1

--- server/middleware/events/listEventsForMonth.js
+++ server/middleware/events/listEventsForMonth.js
@@ -1,24 +1,10 @@
-const models = require('../../persistence/index').models;
-const debug = require('debug')('app:middleware:events:listEventsForMonth');
-const sprintf = require('sprintf-js').sprintf;
-const LeaveEvent = models.LeaveEvent;
+const searcher = require('./../../components/LeaveCalendarSearcher');
 
 module.exports = (req, res) => {
   if (!req.user) {
     res.status(404).json({message: 'No authenticated user found'});
   } else {
-    var myStartDate = new Date();
-    myStartDate.setUTCHours(0, 0, 0, 0);
-    myStartDate.setUTCFullYear(req.params.year, req.params.month - 1, 1);
-
-    var myEndDate = new Date();
-    myEndDate.setUTCHours(0, 0, 0, 0);
-    myEndDate.setUTCFullYear(req.params.year, req.params.month, 1);
-
-    debug(sprintf("Searching for events after %s and before %s", myStartDate.toISOString(), myEndDate.toISOString()));
-
-    // Find events with a startDate >= the first day of the month and and endDate < the last day of the month
-    LeaveEvent.find({startDate: {$gte: myStartDate}, endDate: {$lt: myEndDate}}, (err, data) => {
+    searcher.findEventsInMonth(req.params.year, req.params.month, (err, data) => {
       if (err) {
         res.json({message: "Error loading leave events", detail: err});
       } else {
