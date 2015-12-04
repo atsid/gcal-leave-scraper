e1a08fbf7a0727a8b64976a025d928584b5ed165
Jon McLean <jon.mclean@atsid.com>
2015-12-04 10:24:05 -0800

    Initial tests pass.  #1

--- server/components/CalendarUtils.js
+++ server/components/CalendarUtils.js
@@ -3,8 +3,16 @@ const debug = require('debug')('app:server:components:CalendarUtils');
 require('moment-range');
 
 function findWorkdaysInRange(startDate, endDate, targetMonth, targetYear) {
-  const wholeMonthRange = moment.range(new Date(targetYear, targetMonth, 1), new Date(targetYear, targetMonth + 1, 0, 23, 59, 59));
+  const targetMonthStartDate = new Date(targetYear, targetMonth, 1);
+  const targetMonthEndDate = new Date(targetYear, targetMonth + 1, 0, 23, 59, 59);
 
+  const wholeMonthRange = moment.range(targetMonthStartDate, targetMonthEndDate);
+
+  // Set the start and end times
+  startDate.setUTCHours(0, 0, 0, 0);
+  endDate.setUTCHours(23, 59, 59, 999);
+
+  debug('Start Date %s End Date %s', startDate, endDate);
   debug('Whole month range "%s"', wholeMonthRange.toString());
 
   // Find the part of the date range that is completely within the month.
@@ -13,9 +21,14 @@ function findWorkdaysInRange(startDate, endDate, targetMonth, targetYear) {
 
   range = range.intersect(wholeMonthRange);
 
+  if (range === null) {
+    debug('Dates are not within the specified month');
+    return 0;
+  }
+
   debug('Intersected date range "%s"', range.toString());
 
-  const totalDays = range.diff('d');
+  const totalDays = range.diff('d') + 1;
 
   return totalDays;
 }
--- server/components/CalendarUtils.spec.js
+++ server/components/CalendarUtils.spec.js
@@ -4,7 +4,7 @@ const utils = require('./CalendarUtils');
 describe('The CalendarUtils', () => {
   describe('The findWorkdaysInRange function', () => {
     it('returns the correct number of days when range is within the same week.', (done) => {
-      const result = utils.findWorkdaysInRange(new Date('2015-12-14'), new Date('2015-12-17'), 12, 2015);
+      const result = utils.findWorkdaysInRange(new Date('2015-12-14'), new Date('2015-12-17'), 11, 2015);
 
       expect(result).to.equal(4);
 
