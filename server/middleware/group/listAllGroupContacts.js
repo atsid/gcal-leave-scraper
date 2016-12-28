2363cdf136b5ce2ac8950dd5cd9f2b6cd0d78ef8
Nathan Evans <github@talkswithnumbers.com>
2016-12-28 15:25:33 -0800

    Lint

--- server/middleware/group/listAllGroupContacts.js
+++ server/middleware/group/listAllGroupContacts.js
@@ -4,12 +4,6 @@ function errorNoUserFound(res) {
   res.status(404).json({message: 'No authenticated user found'});
 }
 
-function errorGeneric(res) {
-  // (res, err) {
-  // TODO: Add debug logging for the err message
-  res.status(500).send({ error: 'Unable to fetch all users' });
-}
-
 // This was using hard-coded users in a set of json files. Now it only supports the "all" group, which is everyone in the google domain
 function fetchAllUsers(req, res) {
   contacts.listAllContacts(req, res);
